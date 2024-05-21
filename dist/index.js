5307db55607d763879143004a85ac1ce928c6f4a
sachinda <sachinda@parkwaylabs.com>
2024-05-21 14:20:48 +0530

    pm2 add

--- dist/index.js
+++ dist/index.js
@@ -14,30 +14,29 @@ const jwtOptions = {
 passport.use(new Strategy(jwtOptions, async (payload, done) => {
     const currentTime = Date.now();
     //set 5 minute expair token time
-    if (payload.time + 1000 * 60 * 5 < currentTime) {
-        return done(null, false);
-    }
-    else {
-        const user = await User.findOne({
-            where: { id: payload.id },
+    // if (payload.time + 1000 * 60 * 5 < currentTime) {
+    //   return done(null, false);
+    // } else {
+    const user = await User.findOne({
+        where: { id: payload.id },
+        rows: true,
+    });
+    if (user) {
+        const { RoleId } = user;
+        const Dbroles = await Role.findOne({
+            where: { id: RoleId },
             rows: true,
         });
-        if (user) {
-            const { RoleId } = user;
-            const Dbroles = await Role.findOne({
-                where: { id: RoleId },
-                rows: true,
-            });
-            return done(null, {
-                id: user.id,
-                userName: user.userName,
-                role: Dbroles.role,
-            });
-        }
-        else {
-            return done(null, false);
-        }
+        return done(null, {
+            id: user.id,
+            userName: user.userName,
+            role: Dbroles.role,
+        });
     }
+    else {
+        return done(null, false);
+    }
+    // }
 }));
 app.use("/medication", passport.authenticate("jwt", { session: false }), require("./routes/medication"));
 app.use("/customer", passport.authenticate("jwt", { session: false }), require("./routes/customer"));
